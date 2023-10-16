326017199f2ec7d37463514af76a409d487c4112
beepityboopity <143839769+beepityboopity@users.noreply.github.com>
2023-10-16 17:41:58 -0400

    bsvkvdbjk zsabvjk,

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-block = True
+import keyboard
+from tkinter import *
+import random
 
-if block:
-    print("yee haw")
+
+class Grid:
+    def __int__(self, x, y, empty, label):
+        self.x = x
+        self.y = y
+        self.empty = empty
+        self.label = label
+
+
+class Blocks:
+    def __int__(self, orientations, direction, bottom):
+        self.orientations = orientations
+        self.direction = direction
+        self.bottom = bottom
+
+
+class Tetris(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+
+
+window = Tk()
+stuff = Tetris(window)
+window.geometry("450x500")
+window.title("TETRIS TETRIS TETRIS")
+window.mainloop()
     
