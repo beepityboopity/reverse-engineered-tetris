5f482b512c2a2d6d3d7f12805486cff8e4585e5e
beepityboopity <143839769+beepityboopity@users.noreply.github.com>
2023-11-06 11:31:19 -0500

    IT WORKS NOW ADD THE REST OF IT

--- main.py
+++ main.py
@@ -16,16 +16,18 @@ class GameGrid:
 
 
 class Blocks:
-    def __init__(self, base, rotation1, rotation2, rotation3, bottom):
+    def __init__(self, base, rotation1, rotation2, rotation3, bottom, color):
         self.rotation1 = rotation1
         self.rotation2 = rotation2
         self.rotation3 = rotation3
         self.base = base
         self.bottom = bottom
+        self.color = color
 
 
 class Tetris(Frame):
     block = None
+    colors = ["red", "blue", "green", "pink", "purple", "orange", "yellow"]
 
     def __init__(self, master):
         Frame.__init__(self, master)
@@ -45,7 +47,7 @@ class Tetris(Frame):
             for thing in collection:
                 thing.label = self.canvas.create_rectangle(thing.y*20, thing.x*20, thing.y*20+20, thing.x*20+20, outline="white", fill="black")
 
-        self.block = Blocks([[0, 5], [0, 6], [1, 5], [1, 6]], [[0, 0], [0, 0], [0, 0], [0, 0]], [[0, 0], [0, 0], [0, 0], [0, 0]], [[0, 0], [0, 0], [0, 0], [0, 0]], 1)
+        self.block = Blocks([[0, 5], [0, 6], [1, 5], [1, 6]], [[0, 0], [0, 0], [0, 0], [0, 0]], [[0, 0], [0, 0], [0, 0], [0, 0]], [[0, 0], [0, 0], [0, 0], [0, 0]], 1, random.choice(Tetris.colors))
 
     gaming = False
     def start(self):
@@ -65,36 +67,39 @@ class Tetris(Frame):
                 else:
                     self.block_place()
             if self.block is None:
-                print("making new block")
                 self.block = Blocks([[0, 5], [0, 6], [1, 5], [1, 6]], [[0, 0], [0, 0], [0, 0], [0, 0]],
-                                    [[0, 0], [0, 0], [0, 0], [0, 0]], [[0, 0], [0, 0], [0, 0], [0, 0]], 1)
-            self.after(100, self.game)
+                                    [[0, 0], [0, 0], [0, 0], [0, 0]], [[0, 0], [0, 0], [0, 0], [0, 0]], 1, random.choice(Tetris.colors))
+            self.after(1000, self.game)
 
     def block_place(self):
         for square in self.block.base:
             GameGrid.full_grid[square[0]][square[1]].empty = False
         self.block = Blocks([[0, 5], [0, 6], [1, 5], [1, 6]], [[0, 0], [0, 0], [0, 0], [0, 0]],
-                                    [[0, 0], [0, 0], [0, 0], [0, 0]], [[0, 0], [0, 0], [0, 0], [0, 0]], 1)
+                                    [[0, 0], [0, 0], [0, 0], [0, 0]], [[0, 0], [0, 0], [0, 0], [0, 0]], 1, random.choice(Tetris.colors))
 
         for row in GameGrid.full_grid:
-            all = 0
+            temp = 10
             for obj in row:
-                if not obj.empty:
-                    all += 1
-            if all == 10:
-                temp = GameGrid.full_grid.index(row)
-                for x in range(temp):
-                    GameGrid.full_grid[temp] = GameGrid.full_grid[temp - x]
+                if obj.empty:
+                    temp -= 1
 
-    def is_empty(self, obj):
-        if obj.empty:
-            return False
+            if temp == 10:
+                row_index = GameGrid.full_grid.index(row)
+                self.falling(row_index)
+
+    def falling(self, count):
+        if count == 0:
+            return
         else:
-            return True
+            for obj in GameGrid.full_grid[count]:
+                obj_index = GameGrid.full_grid[count].index(obj)
+                obj.empty = GameGrid.full_grid[count - 1][obj_index].empty
+                color = self.canvas.itemcget(GameGrid.full_grid[count - 1][obj_index].label, "fill")
+                self.canvas.itemconfig(obj.label, fill=color)
+            self.falling(count-1)
     def move_down(self):
         truth = 0
         for a in self.block.base:
-            print(a)
             if GameGrid.full_grid[a[0]+1][a[1]].empty:
                 truth += 1
             else:
@@ -106,7 +111,7 @@ class Tetris(Frame):
                 a[0] += 1
             self.block.bottom += 1
 
-    def move_side(self, event):
+    def move_keys(self, event):
         if event.keysym in ["Left", "Right", "a", "d"]:
             if event.keysym == "Left" or event.keysym == "a":
                 direction = -1
@@ -122,9 +127,12 @@ class Tetris(Frame):
                     self.canvas.itemconfig(GameGrid.full_grid[a[0]][a[1]].label, fill="black")
                     a[1] += direction
 
+        elif event.keysym == "Up":
+            print("weuweuwueuwueuwueu")
+
     def display(self):
         for square in self.block.base:
-            self.canvas.itemconfig(GameGrid.full_grid[square[0]][square[1]].label, fill="red")
+            self.canvas.itemconfig(GameGrid.full_grid[square[0]][square[1]].label, fill=self.block.color)
         self.after(1, self.display)
 
 
@@ -132,6 +140,6 @@ window = Tk()
 stuff = Tetris(window)
 window.geometry("450x510")
 window.title("TETRIS TETRIS TETRIS")
-window.bind("<KeyRelease>", stuff.move_side)
+window.bind("<KeyRelease>", stuff.move_keys)
 window.mainloop()
     
